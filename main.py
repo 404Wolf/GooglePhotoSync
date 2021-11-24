8b0e65eec54ce4d5c934da5b7b5ce747ea40a24d
Twilak <wolfmermelstein@gmail.com>
2021-11-24 11:48:07 -0500

    google_photos() auth works

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ import aiohttp
 import aiofiles
 import sys
 import json
+import os
+from datetime import datetime
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 # set the event policy to windows if the user is using windows
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-class googlePhotos():
-    def __init__(self,client_id,client_secret,project_id):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.project_id = project_id
-        self.headers = {
-            "client_id":self.client_id,
-            "client_secret":self.client_secret,
-            "project_id":self.project_id
-        }
+class google_photos():
+    def __init__(self,auth_file):
         self.session = aiohttp.ClientSession()
 
-    async def auth_account(self):
-        async with self.session.put("https://www.googleapis.com/auth/photoslibrary.readonly",headers=self.headers) as resp:
-            print(await resp.text())
+        SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+        API_SERVICE_NAME = 'photoslibrary'
+        API_VERSION = 'v2'
+        CLIENT_SECRETS_FILE = auth_file
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        self.auth = flow.run_console()
+        self.refresh_token = self.auth._refresh_token
+        self.access_token = self.auth.token
+        self.expire_timestamp = datetime.timestamp(self.auth.expiry)
+        self.headers = {"Authorization":"Bearer "+self.access_token}
+        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     async def list_albums(self):
         async with self.session.get("https://photoslibrary.googleapis.com/v1/albums",headers=self.headers) as resp:
@@ -39,11 +46,8 @@ async def main():
         auth_file = json.load(auth_file)
 
     try:
-        client = googlePhotos(auth_file["client_id"],auth_file["client_secret"],auth_file["project_id"])
-        await client.auth_account()
-        # albums = await client.list_albums()
-        # print(albums)
-
+        client = google_photos("auth.json")
+        print(await client.list_albums())
     finally:
         await client.close_session()
 
