3a9941c94eef8ebacfe01e06d1e27c329545a758
Wolf <Wolf@techy.cc>
2021-11-25 00:36:31 -0500

    Added docstrings and .request(endpoint,scope) function

---
+++ google_api.py
@@ -0,0 +1,184 @@
+import aiohttp
+import pkce
+import json
+from webbrowser import open_new
+from time import time
+
+
+class google_api:
+    """
+    Class to access google api endpoints with aiohttp and asyncio.
+
+    Includes functions for gathering auth information/authing the account, and for accessing endpoints
+
+    Attributes:
+        appdata:
+            appdata["client_id"]: client id for google app
+            appdata["project_id"]: project id for google app
+            appdata["client_secret"]: client secret for google app
+        auth: a dict with information for authing the account.
+            "scope" indicates which scope auth information is for
+            auth["https://www.googleapis.com/auth/this.is.a.scope"]["access_token"]: for sending requests to google api
+            auth["https://www.googleapis.com/auth/this.is.a.scope"]["refresh_token"]: for regenerating access tokens on the fly
+            auth["https://www.googleapis.com/auth/this.is.a.scope"]["expires_at"]: UNIX timestamp for when the access_token for the given scope expires
+    """
+
+    def __init__(self, auth_file="auth.json"):
+        """
+        Creates aiohttp client session for async web requests, and stores auth_file name
+
+        Args:
+            auth_file: name of auth file (string)
+        Returns:
+            None
+        """
+
+        self.scopes_file = auth_file
+        self.session = aiohttp.ClientSession()
+
+    def load_auth_file(self):
+        """
+        Reads the auth_file and returns it's contents as a json
+
+        Args:
+            None
+
+        Returns:
+            dict of the read auth_file
+
+        Raises:
+            FileNotFoundError: file with the name of auth_file no longer exists
+        """
+        return json.load(open(self.scopes_file))
+
+    def dump_auth_file(self, dict):
+        """
+        Dump a dict to the auth file json
+
+        Args:
+            dict
+
+        Returns:
+            None
+
+        Raises:
+            FileNotFoundError: file with the name of auth_file no longer exists
+        """
+        json.dump(dict, open(self.scopes_file, "w"), indent=3)
+
+    async def auth(self, scope, open_in_browser=True):
+        """
+        Update the self.scopes attribute (obtain valid access_tokens for accessing google api)
+
+        Either uses the already existent refresh_token in the <auth_file>.json, or gathers a new one, and then
+        uses it to generate an access token for a given scope.
+
+        Args:
+            scope: the google authentication scope to get an access_token for
+            open_in_browser: whether to open a web broswer on the user's pc if a new refresh token is needed
+
+        Returns:
+            None (updates class attributes and auth file)
+        """
+
+        auth_file = self.load_auth_file()
+        self.appdata = auth_file["appdata"]
+        self.scopes = auth_file["scopes"]
+
+        if scope not in self.scopes:
+            self.scopes[scope] = {}
+
+        # pkce token to secure requests
+        code_verifier = pkce.generate_code_verifier(length=128)
+        code_challenge = pkce.get_code_challenge(code_verifier)
+
+        # once user finishes auth steps provided by google's website, they will be given a code direcly with this uri
+        redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+
+        # set up params for request to get token
+        params = {
+            "client_id": self.appdata["client_id"],
+            "client_secret": self.appdata["client_secret"],
+        }
+
+        if "refresh_token" not in self.scopes[scope]:
+            params["code_verifier"] = code_verifier
+            params["grant_type"] = "authorization_code"
+            params["redirect_uri"] = redirect_uri
+
+            # generate a url for the user to go to to auth their account
+            auth_url = (
+                "https://accounts.google.com/o/oauth2/v2/auth"
+                + "?client_id="
+                + self.appdata["client_id"]
+                + "&scope=https://www.googleapis.com/auth/"
+                + scope
+                + "&response_type=code"
+                + "&redirect_uri=urn:ietf:wg:oauth:2.0:oob"
+                + "&code_challenge="
+                + code_challenge
+                + "&code_challenge_method=S256"
+            )
+
+            # open it in a web browser
+            if open_in_browser:
+                open_new(auth_url)
+
+            # prompt user for code google gives them after they finish the auth steps
+            params["code"] = input(
+                "Enter\n"
+                + auth_url
+                + "\ninto a web browser, and paste the code google gives you here: "
+            )
+        else:
+            params["refresh_token"] = self.scopes[scope]["refresh_token"]
+            params["grant_type"] = "refresh_token"
+
+        # make request to google to get token
+        async with self.session.post(
+            "https://oauth2.googleapis.com/token", params=params
+        ) as resp:
+
+            resp = await resp.json()
+            # if this is a new request with a user provided code, then store the refresh_token
+            if "refresh_token" not in self.scopes[scope]:
+                self.scopes[scope]["refresh_token"] = resp["refresh_token"]
+
+            # otherwise, there shouldn't be any refresh token in the response
+            self.scopes[scope]["access_token"] = resp["access_token"]
+
+            # store the expire timestamp for the access token
+            self.scopes[scope]["expires_at"] = round(time() + resp["expires_in"]) - 1
+
+        # save auth information to json file
+        self.dump_auth_file({"scopes": self.scopes, "appdata": self.appdata})
+
+    async def request(self, endpoint, scope):
+        """
+        Make a request to a google api endpoint
+
+        Args:
+            endpoint: the endpoint to gather data from (to proform a GET request to)
+            scope: the scope needed to access that endpoint
+
+        Returns:
+            either response as a dictonary, or status code if status code != 200
+        """
+
+        if self.scopes[scope]["expires_at"] < time():
+            await self.scopes(scope)
+
+        headers = {"Authorization": "Bearer " + self.scopes[scope]["access_token"]}
+        async with self.session.get(
+            "https://photoslibrary.googleapis.com/v1/" + endpoint, headers=headers
+        ) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            else:
+                return resp.status
+
+    async def close_session(self):
+        """
+        Closes aiohttp session
+        """
+        await self.session.close()
--- main.py
+++ main.py
@@ -1,82 +1,20 @@
 import asyncio
-import aiohttp
-import aiofiles
 import sys
-import json
-import os
-import requests
-from datetime import datetime
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_api import google_api
 
-# set the event policy to windows if the user is using windows
+# set the event policy to prevent windows bugs
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-class google_photos():
-    def __init__(self,auth_file):
-        self.session = aiohttp.ClientSession()
-
-        try:
-            with open(auth_file) as auth_file_contents:
-                auth_file_contents = json.load(auth_file_contents)
-            self.client_id = auth_file_contents["installed"]["client_id"]
-            self.client_secret = auth_file_contents["installed"]["client_secret"]
-            self.refresh_token = auth_file_contents["refresh_token"]
-            self.refresh_client()
-        except:
-            SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-            API_SERVICE_NAME = 'photoslibrary'
-            API_VERSION = 'v2'
-            CLIENT_SECRETS_FILE = auth_file
-            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-            auth = flow.run_console()
-            self.client_id = auth._client_id
-            self.client_secret = auth._client_secret
-            self.refresh_token = auth._refresh_token
-            self.access_token = auth.token
-            self.expire_timestamp = datetime.timestamp(auth.expiry)
-            os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-            with open(auth_file) as auth_file_contents:
-                auth_file_contents = json.load(auth_file_contents)
-                auth_file_contents["refresh_token"] = self.refresh_token
-            with open(auth_file,"w") as auth_file_contents_out:
-                json.dump(auth_file_contents,auth_file_contents_out,indent=3)
-
-    def refresh_client(self):
-        headers = {
-            "client_id":self.client_id,
-            "client_secret":self.client_secret,
-            "code":self.refresh_token,
-            "grant_type":"authorization_code"
-        }
-        resp = requests.post("https://oauth2.googleapis.com/token",headers=headers).json()
-        print(resp)
-        self.access_token = resp["access_token"]
-        self.expire_timestamp = resp["expires_in"]
-
-    async def list_albums(self):
-        headers = {"Authorization":"Bearer "+self.access_token}
-        async with self.session.get("https://photoslibrary.googleapis.com/v1/albums",headers=headers) as resp:
-            if resp.status == 200:
-                return await resp.json()
-            else:
-                return resp.status
-
-    async def close_session(self):
-        await self.session.close()
 
 async def main():
-    with open("auth.json") as auth_file:
-        auth_file = json.load(auth_file)
-
     try:
-        client = google_photos("auth.json")
-        print(await client.list_albums())
+        client = google_api()
+        await client.auth("photoslibrary.readonly")
+        data = await client.request("mediaItems","photoslibrary.readonly")
+        print(data)
     finally:
         await client.close_session()
+
 
 asyncio.run(main())
